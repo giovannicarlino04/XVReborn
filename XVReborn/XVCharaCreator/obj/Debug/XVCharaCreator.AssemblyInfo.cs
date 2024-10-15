b093f30484b4591b5ea0bc4d67bb2f3bda1b60a3
Giovanni Carlino <97813246+giovannicarlino04@users.noreply.github.com>
2024-10-15 14:45:39 +0200

    dd

--- XVReborn/XVCharaCreator/obj/Debug/XVCharaCreator.AssemblyInfo.cs
+++ XVReborn/XVCharaCreator/obj/Debug/XVCharaCreator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XVCharaCreator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da00121d691f1969c4c34eab741e3d3bffa78d9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb7703af4ad28c7fe2bd9a9d6be3e71f4ab81b39")]
 [assembly: System.Reflection.AssemblyProductAttribute("XVCharaCreator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XVCharaCreator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
